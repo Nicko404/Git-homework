1a6966bd479ca9d7e938d032ca27fdb387329db4
Nicko404 <kolsak2013@yandex.ru>
2024-08-30 00:54:22 +0300

    Some improvements

--- src/main/java/com/example/service/Main.java
+++ src/main/java/com/example/service/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("I want to say hello to this world");
         System.out.println("Application is ready for realise");
+        System.out.println("Now is ready for realise");
     }
 
     public static void someLogic() {
